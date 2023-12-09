78ff1997df2d847b78578970f7192451ee226b2c
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-09 14:58:45 +0000

    actualizacion 2

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 
 const cards = [
-     { imageUrl: "1", title: "John Doe", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
-     { imageUrl: "1", title: "John 2", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
-     { imageUrl: "1", title: "John 3", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
-     { imageUrl: "1", title: "John 4", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
+     { imageUrl: "https://placehold.co/300x200/EEE/31343C", title: "John Doe", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
+     { imageUrl: "https://placehold.co/300x200/EEE/31343C", title: "John 2", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
+     { imageUrl: "https://placehold.co/300x200/EEE/31343C", title: "John 3", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
+     { imageUrl: "https://placehold.co/300x200/EEE/31343C", title: "John 4", description: "johndoe@example.com" , buttonUrl: "johndoe@example.com", buttonLabel: "johndoe@example.com"},
    ];
 
-const cardArray = cards;
+
 
 //create your first component
 const Card = () => {
 	return (
-          
+
+        <div className="container my-5">
+          <div className="row">
                     <>
                     {cards.map(function(card){
                         return (
                         
-                                        <div classNameName="card" style={{width: "18rem"}}>
-                                                    <img src={card.imageUrl} classNameName="card-img-top" alt="..."/>
-                                                    <div classNameName="card-body">
-                                                        <h5 classNameName="card-title">{card.title}</h5>
-                                                        <p classNameName="card-text">{card.description}</p>
-                                                        <a href={card.buttonUrl} classNameName="btn btn-primary">{card.buttonLabel}</a>
+                                        <div className="card mx-2" style={{width: "18rem"}}>
+                                                    <img src={card.imageUrl} className="card-img-top" alt="..."/>
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">{card.title}</h5>
+                                                        <p className="card-text">{card.description}</p>
+                                                        <a href={card.buttonUrl} className="btn btn-primary">{card.buttonLabel}</a>
                                                     </div>
                                         </div>
                                                     
@@ -30,8 +32,8 @@ const Card = () => {
 
                     )})}
                     </>
-
-       
+            </div>
+            </div>
 
 
 	);
--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="container-fluid bg-dark">
             <footer className="footer mt-5 py-5">
             <div className="container text-center text-light">
-                <p className="mb-0">2023 - creado por XXX</p>
+                <p className="mb-0">2023 - creado por 4GeeK</p>
             </div>
             </footer>
         </div>
